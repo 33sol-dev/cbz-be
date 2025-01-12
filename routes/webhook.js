9bd157f8d4b7a09dadfb4025b53498bae5175e74
33-sol <33sol@33-sol.com>
2025-01-12 14:37:10 +0530

    ndhdf

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const { handleIncomingMessage } = require('../controllers/webhookController');
-
+const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
 const router = express.Router();
 
 // Define the webhook route
 router.post('/', handleIncomingMessage);
 router.get('/', (req, res) => {
-    res.status(200).send('Webhook is working!');
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+  
+    if (mode && token) {
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        logger.info('Webhook verified successfully');
+        res.status(200).send(challenge);
+      } else {
+        logger.warn('Webhook verification failed');
+        res.sendStatus(403);
+      }
+    } else {
+      res.sendStatus(400);
+    }
+
 });
 module.exports = router;
