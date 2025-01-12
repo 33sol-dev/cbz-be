056401dc51560fcf584faeb10b9f917ee57033d2
33-sol <33sol@33-sol.com>
2025-01-12 10:40:02 +0530

    so

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,8 +23,7 @@ module.exports = {
         host: "ec2-44-211-205-51.compute-1.amazonaws.com", // EC2 instance public DNS
         key: "CBZ-BE.pem", // Path to your private SSH key
         ref: "origin/main",
-        repo: "git@github.com:33sol-dev/cbz-be.git", // Your GitHub repo
-        path: "/home/ec2-user/cbz-be", // Deployment directory on the EC2 instance
+        repo: "git@github.com:33sol-dev/cbz-be.git",
         "pre-deploy-local": "",
         "post-deploy": "npm install && pm2 reload /home/ec2-user/cbz-be/ecosystem.config.js --env production"
       },
