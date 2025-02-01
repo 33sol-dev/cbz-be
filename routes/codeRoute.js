dfa81a49ff822a42ff8ad346bb9d0f2e630167c2
33sol <33sol@33-sol.com>
2025-02-01 09:03:33 +0530

    updated backend

--- routes/codeRoute.js
+++ routes/codeRoute.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const router = express.Router();
-
-
-module.exports = router;
+const {
+    processQrScan,
+    taskCompletion,
+    getQrByCampaign,
+  } = require("../controllers/codeController");
+  
+  const router = require("express").Router();
+  
+  router.get("/:code", processQrScan);
+  router.post("/complete-task", taskCompletion);
+  router.post("/get-campaign-codes", getQrByCampaign);
+  
+  module.exports = router;
