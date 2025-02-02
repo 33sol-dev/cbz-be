279998be28aeff064d1f6d3ef365ae25262a6f6d
Khanba22 <khanba2222khan@gmail.com.com>
2025-02-02 22:50:46 +0530

    Updated Env

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,20 +1,32 @@
 // routes/campaigns.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const campaignController = require('../controllers/campaignController');
+const campaignController = require("../controllers/campaignController");
 
-const { verifyToken } = require('../middlewares/auth');
-const multer = require('multer');
-const { generateCampaign } = require('../controllers/campaignCreator');
-const upload = multer({ dest: 'uploads/' }); 
+const { verifyToken } = require("../middlewares/auth");
+const multer = require("multer");
+const { generateCampaign } = require("../controllers/campaignCreator");
+const upload = multer({ dest: "uploads/" });
 
 // router.post('/create', upload.single('file'), campaignController.createCampaign);
-router.post('/create', upload.single('file'), generateCampaign);
-router.get('/',  campaignController.getCampaigns);
-router.get('/:campaignId', verifyToken, campaignController.getCampaignById);
-router.get('/:campaignId/insights', verifyToken, campaignController.getCampaignInsights);
-router.post('/:campaignId/publish', verifyToken, campaignController.publishCampaign);
-router.get('/:campaignId/summary', verifyToken, campaignController.getCampaignSummary);
+router.post("/create", upload.single("file"), generateCampaign);
+router.get("/", verifyToken, campaignController.getCampaigns);
+router.get("/:campaignId", verifyToken, campaignController.getCampaignById);
+router.get(
+  "/:campaignId/insights",
+  verifyToken,
+  campaignController.getCampaignInsights
+);
+router.post(
+  "/:campaignId/publish",
+  verifyToken,
+  campaignController.publishCampaign
+);
+router.get(
+  "/:campaignId/summary",
+  verifyToken,
+  campaignController.getCampaignSummary
+);
 
 module.exports = router;
