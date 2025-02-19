08424e7ebd6e12aaf4b71ed10733975e933c87f5
33-sol <33sol@33-sol.com>
2025-02-19 22:31:25 +0530

    rate

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.use(mongoSanitize());
 // Rate Limiting Middleware
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 500, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
